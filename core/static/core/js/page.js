3f43620bfbf929730a0b4f13db16fe7cbee5e3e8
Bruno Martinho <leonardo.bruno9203@gmail.com>
2023-06-02 22:47:50 -0300

    Falta Imagem e funcionar calendário

--- core/static/core/js/page.js
+++ core/static/core/js/page.js
@@ -8,3 +8,14 @@ Alpine.plugin(mask);
 Alpine.start();
 
 tippy("#home-icon", { content: "Dashboard", placement: "bottom" });
+
+// Função para abri e fechar o "Calendário" da Navbar lateral
+function toggleCalendar() {
+  var calendarContent = document.getElementById("calendar-content");
+  
+  if (dropdownContent.classList.contains("hidden")) {
+    dropdownContent.classList.remove("hidden"); // Mostra a dropdown
+  } else {
+    dropdownContent.classList.add("hidden"); // Oculta a dropdown 
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
       colors: {
         "color-primary": "#32659A", //Blue Color
         "color-secundary": "#E2A161", //Orange Color
-        "main-color": "#E6E3E3" //Background Color
+        "main-color": "#E6E3E3", //Background Color
+        "around-color": "#CD9028", //HOVER Color
       }
     },
   },
