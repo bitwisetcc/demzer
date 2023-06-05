a032d0e161626f9b062a20c64447f4ee142a4f66
Jorge Terence <jorge.terence@outlook.com>
2023-06-04 21:09:13 -0300

    Switched import students view to its own app

--- backend/urls.py
+++ backend/urls.py
@@ -1,24 +1,17 @@
 """
-URL configuration for backend project.
+`urlpatterns` routes URLs to views.
+Docs: https://docs.djangoproject.com/en/4.2/topics/http/urls/
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+Class-based views:
+    - Add an import:  from other_app.views import Home
+    - Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 """
 
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("core.urls"))
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
+    path("management/", include("management.urls")),
 ]
