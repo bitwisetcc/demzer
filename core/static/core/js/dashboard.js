f1f54adc4eb7a959dda50341bac335db8b03b84b
Jorge Terence <jorge.terence@outlook.com>
2023-06-05 22:20:27 -0300

    Added basic table logic

--- core/static/core/js/dashboard.js
+++ core/static/core/js/dashboard.js
@@ -1,6 +1,31 @@
 document.addEventListener("alpine:init", () => {
   Alpine.data("dashboard", () => ({
-    active: "students",
-    
+    active: "Alunos",
+    headers: ["RM", "Nome", "Idade", "Gênero", "Celular", "E-mail", "CPF", "RG"],
+    /**
+     * @param {Event} e
+     */
+    reload(e) {
+      this.active = e.target.innerText;
+      const csrfToken = document.cookie.slice(document.cookie.indexOf("=") + 1);
+      
+      switch (e.target.dataset.section) {
+        case "students":
+          this.headers = ["RM", "Nome", "Idade", "Gênero", "Celular", "E-mail", "CPF", "RG"];
+          break;
+      
+        case "courses":
+          this.headers = ["Código", "Nome", "Descrição"];
+          break;
+      
+        default:
+          break;
+      }
+
+      fetch(e.target.dataset.url, { headers: { "X-CSRFToken": csrfToken } })
+        .then((res) => res.json())
+        .then(console.log)
+        .catch(console.error);
+    },
   }));
 });
--- management/urls.py
+++ management/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from management.views import *
 
 urlpatterns = [
-    path("import/students/", import_students, name="management/students"),
     path("query/students/", students, name="query/students"),
+    path("import/students/", import_students, name="management/students"),
 ]
