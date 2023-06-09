be460fcff34fb9cc091fe8a78b074d924e7bdafe
Walrus01 <giovanni.cicero3@gmail.com>
2023-06-09 14:57:33 -0300

    Conserto do background

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,12 @@ module.exports = {
   content: ["**/templates/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "background": "{% static 'core/images/BGDemzer.png' %}"
+      },
       colors: {
-        "color-primary": "#32659A"
+        "color-primary": "#32659A",
+        "color-secondary": "#cd9028"
       }
     },
   },
