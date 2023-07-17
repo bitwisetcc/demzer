57579000bdf87fe4161e883192bd44b37e596682
Walrus01 <giovanni.cicero3@gmail.com>
2023-07-17 14:26:10 -0300

    Finished reporte

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,23 @@ module.exports = {
         "color-primary-dimm": "#0a3c6f",
         "color-secondary": "#E6A82E",
         "color-secondary-dimm": "#E8C17E" 
-      }
+      },
+      screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
     },
   },
   plugins: [require("@tailwindcss/forms")],
