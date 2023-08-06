bae38dda8ac7bd1419b3c1c4ac2d1fc576a2838e
Bruno Martinho <leonardo.bruno9203@gmail.com>
2023-08-06 11:31:53 -0300

    teste

--- core/static/core/js/filter.js
+++ core/static/core/js/filter.js
@@ -1,3 +1,4 @@
+//FOR ALL FILTERS
 document.getElementById("openModalButton").addEventListener("click", function () {
   document.getElementById("modal").classList.remove("hidden");
   document.getElementById("modal").classList.add("flex");
@@ -7,3 +8,4 @@ document.getElementById("closeModalButton").addEventListener("click", function (
   document.getElementById("modal").classList.add("hidden");
   document.getElementById("modal").classList.remove("flex");
 });
+
