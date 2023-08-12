26815ec9d2e19cac6c7a6f44dbaec1502eba8943
Bruno Martinho <leonardo.bruno9203@gmail.com>
2023-08-12 15:26:08 -0300

    reset password created

--- backend/settings.py
+++ backend/settings.py
@@ -149,3 +149,12 @@ AUTH_USER_MODEL = "auth.User"
 LOGIN_URL = "/login/"
 
 ROLEPERMISSIONS_MODULE = "core.roles"
+
+#SMTP Config
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'socialdemzer@gmail.com'
+EMAIL_HOST_PASSWORD = 'Demzer@BGJRS'
--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.shortcuts import redirect
 from django.urls import include, path
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from . import views
 
 from core.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path("", lambda _: redirect("dashboard"), name="empty"),
     path("dashboard/", dashboard, name="dashboard"),
     path("secret/", super_secret, name="secret"),
@@ -17,7 +18,13 @@ urlpatterns = [
     path("comunicados/", comunicados, name="comunicados"),
     path("perfil/", perfil, name="profile"),
     path("boletim/", boletim, name="boletim"),
-    path("user/picture", profile_picture, name="profile_picture")
+    path("user/picture", profile_picture, name="profile_picture"),
+
+    #RESET PASSOWRD
+    path("reset_password/", auth_views.PasswordResetView.as_view(template_name="core/password/password_reset_form.html"), name="password_reset"),
+    path("reset_password/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("reset/<uid64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="passowrd_reset_confirm"),
+    path("reset/done/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_complete")
 ]
 
 
