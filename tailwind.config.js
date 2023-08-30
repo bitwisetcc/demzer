a04c3ae0b1296a952297528f236ba2b219effb64
Jorge Terence <jorge.terence@outlook.com>
2023-08-30 10:34:43 -0300

    fixed color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,30 +4,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "color-primary": "#32659A",
-        "color-primary-dimm": "#365075",
-        "color-secondary": "#E6A82E",
-        "color-txt-perfil": "#054499",
+        "color-primary": "#054499",
+        "color-primary-dimm": "#4373b6",
+
+        "color-secondary": "#CD9028",
+        "color-hover": "#E8C17E",
+
         "color-bg-perfil": "#F5F5F5",
         "color-boletim": "#D7D4D4",
-        "color-name-boletim": "#4A73AC",
-        "color-dark-orange": "#CD9028",
         "color-grey": "#EDECEC",
-        "color-hover": "#E8C17E",
-      }
-    },
-    fontFamily: {
-      'glacial': ['Glacial'],
-      'mono': ["ui-monospace", "SFMono-Regular", "Menlo", "Monaco", "Consolas", "Liberation Mono", "Courier New", "monospace"],
+      },
     },
     screens: {
-      'md': '768px',
-      'sm': '640px',
-      'md2': {'max': '767px'},
-      'sm2': {'max': '375px'},
-      'ultrawide': '2500px',
+      md: "768px",
+      sm: "640px",
+      md2: { max: "767px" },
+      sm2: { max: "375px" },
+      ultrawide: "2500px",
     },
   },
   plugins: [require("@tailwindcss/forms")],
-}
-
+};
